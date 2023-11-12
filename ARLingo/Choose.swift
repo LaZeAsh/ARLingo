704534660bb0c6ee1604a6178d3e5dc7933ec7c6
Ayush <ayushrgarg@gmail.com>
2023-11-12 01:28:58 -0800

    Final commit before OpenAI integration

---
+++ ARLingo/Choose.swift
@@ -0,0 +1,8 @@
+//
+//  Choose.swift
+//  ARLingo
+//
+//  Created by Ash on 11/12/23.
+//
+
+import Foundation
---
+++ ARLingo/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  ARLingo
+//
+//  Created by Ash on 11/12/23.
+//
+
+import Foundation
